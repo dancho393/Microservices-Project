f696007977339a8e4af84e394b0e920da719bc2d
dancho393 <danez7000@gmail.com>
2024-09-24 12:40:41 +0200

    CreateProductService create Product and save it into the repo

---
+++ src/main/java/com/learning/microservices/product/service/CreateProductService.java
@@ -0,0 +1,29 @@
+package com.learning.microservices.product.service;
+
+
+import com.learning.microservices.product.dto.product.create.CreateProductOperation;
+import com.learning.microservices.product.dto.product.create.CreateProductRequest;
+import com.learning.microservices.product.dto.product.create.CreateProductResponse;
+import com.learning.microservices.product.model.Product;
+import com.learning.microservices.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CreateProductService implements CreateProductOperation {
+    private final ProductRepository productRepository;
+
+
+    @Override
+    public CreateProductResponse process(CreateProductRequest request) {
+        Product product = Product.builder()
+                .id(request.id())
+                .description(request.description())
+                .name(request.name())
+                .price(request.price())
+                .build();
+        productRepository.save(product);
+        return new CreateProductResponse("Product created");
+    }
+}
