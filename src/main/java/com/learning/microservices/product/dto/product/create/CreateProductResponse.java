046ef825c2600d24ac9b07b44a85cb44832e50f3
dancho393 <danez7000@gmail.com>
2024-09-24 12:40:18 +0200

    CreateProductResponse return message(String)

--- src/main/java/com/learning/microservices/product/dto/product/create/CreateProductResponse.java
+++ src/main/java/com/learning/microservices/product/dto/product/create/CreateProductResponse.java
@@ -3,6 +3,6 @@ package com.learning.microservices.product.dto.product.create;
 import com.learning.microservices.product.dto.base.OperationResponse;
 
 public record CreateProductResponse(
-
+    String message
 ) implements OperationResponse {
 }
