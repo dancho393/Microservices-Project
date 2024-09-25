99f69425c87c27d9a966d46e812adb3c22f3804a
dancho393 <danez7000@gmail.com>
2024-09-25 13:33:36 +0200

    fixed bug that create product wasnt returning CREATED status

--- src/main/java/com/learning/microservices/product/controller/ProductController.java
+++ src/main/java/com/learning/microservices/product/controller/ProductController.java
@@ -3,8 +3,13 @@ package com.learning.microservices.product.controller;
 
 import com.learning.microservices.product.dto.product.create.CreateProductRequest;
 import com.learning.microservices.product.dto.product.create.CreateProductResponse;
-import com.learning.microservices.product.service.CreateProductService;
+import com.learning.microservices.product.dto.product.getall.GetAllProductsRequest;
+import com.learning.microservices.product.dto.product.getall.GetAllProductsResponse;
+import com.learning.microservices.product.model.Product;
+import com.learning.microservices.product.service.product.CreateProductService;
+import com.learning.microservices.product.service.product.GetAllProductsService;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class ProductController {
     private final CreateProductService createProductService;
+    private final GetAllProductsService getAllProductsService;
 
 
     @PostMapping
@@ -21,6 +27,13 @@ public class ProductController {
     public ResponseEntity<CreateProductResponse> createProduct(
             @RequestBody CreateProductRequest productRequest
     ) {
-        return ResponseEntity.ok().body(createProductService.process(productRequest));
+        return ResponseEntity.status(HttpStatus.CREATED).body(createProductService.process(productRequest));
+    }
+    @GetMapping("/{page}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<GetAllProductsResponse> getAllProducts(
+            @PathVariable int page) {
+        GetAllProductsRequest request = new GetAllProductsRequest(page);
+        return ResponseEntity.ok().body(getAllProductsService.process(request));
     }
 }
