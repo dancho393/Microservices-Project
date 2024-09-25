0fab3979d1f4839dfaea43dbb7a6f171a2ead99f
dancho393 <danez7000@gmail.com>
2024-09-25 13:34:13 +0200

    added test to the create product service

--- src/main/java/com/learning/microservices/product/ProductServiceApplication.java
+++ src/main/java/com/learning/microservices/product/ProductServiceApplication.java
@@ -2,8 +2,12 @@ package com.learning.microservices.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/learning/microservices/product/ProductServiceApplicationTests.java
+++ src/test/java/com/learning/microservices/product/ProductServiceApplicationTests.java
@@ -1,15 +1,55 @@
 package com.learning.microservices.product;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
+    @ServiceConnection
+    static MongoDBContainer mongoDBContainer=new MongoDBContainer("mongo:7.0.5");
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setup(){
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+    static {
+        mongoDBContainer.start();
+
+    }
     @Test
-    void contextLoads() {
+    void shouldCreateProduct() {
+        String requestBody = """
+                {
+                    "name":"Iphone 17 Pro Max",
+                    "description":"It is amazing",
+                    "price":"1655.5"
+                }
+                """;
+        RestAssured.given()
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post("api/product")
+                .then()
+                .statusCode(201)
+                .body("message", Matchers.equalTo("Product created"))
+                .body("id", Matchers.notNullValue())
+                .body("name", Matchers.equalTo("Iphone 17 Pro Max"))
+                .body("description", Matchers.equalTo("It is amazing"))
+                .body("price", Matchers.equalTo("1655.5"));
+
     }
 
 }
