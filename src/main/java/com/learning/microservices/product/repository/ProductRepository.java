b332bcdd00b798c2a25d14f8a4b29acfca514cf8
dancho393 <danez7000@gmail.com>
2024-09-25 13:33:56 +0200

    added pageable method to find all products in the current page

--- src/main/java/com/learning/microservices/product/repository/ProductRepository.java
+++ src/main/java/com/learning/microservices/product/repository/ProductRepository.java
@@ -1,7 +1,10 @@
 package com.learning.microservices.product.repository;
 
 import com.learning.microservices.product.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ProductRepository extends MongoRepository<Product, String> {
+    Page<Product> findAll(Pageable pageable);
 }
